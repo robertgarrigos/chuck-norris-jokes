5838059853f2e55d11fdd2fe6247d3536722e76c
Robert Garrigos <robert@garrigos.cat>
2019-07-08 16:08:57 +0200

    Apply fixes from StyleCI (#1)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -8,7 +8,7 @@ class JokeFactory
         'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
         'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
         'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-        'Chuck Norris counted to infinity... Twice.'
+        'Chuck Norris counted to infinity... Twice.',
     ];
 
     public function __construct(array $jokes = null)
