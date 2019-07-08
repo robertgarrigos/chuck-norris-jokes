7543aab66f61974dad4e30e0efeba1e40f37dd26
Robert Garrigos <robert@garrigos.cat>
2019-07-08 14:08:50 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -5,7 +5,7 @@ namespace RobertGarrigos\ChuckNorrisJokes\Tests;
 use PHPUnit\Framework\TestCase;
 use RobertGarrigos\ChuckNorrisJokes\JokeFactory;
 
-class JokeFactoryTest extends TestCase  
+class JokeFactoryTest extends TestCase
 {
     /** @test */
     public function it_returns_a_random_joke()
@@ -16,22 +16,21 @@ class JokeFactoryTest extends TestCase
         $joke = $jokes->getRandomJoke();
 
         $this->assertSame('This is a Joke', $joke);
-    }  
-    
+    }
+
     /** @test */
     public function it_returns_a_predefined_joke()
     {
-
         $chuckNorrisJokes = [
             'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
             'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
             'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-            'Chuck Norris counted to infinity... Twice.'
+            'Chuck Norris counted to infinity... Twice.',
         ];
 
         $jokes = new JokeFactory();
         $joke = $jokes->getRandomJoke();
 
         $this->assertContains($joke, $chuckNorrisJokes);
-    }  
+    }
 }
